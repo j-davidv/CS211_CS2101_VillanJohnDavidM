fa5a808ee450f51b1d97a2b8c495da0a636b5b46
j-davidv <david019villan@gmail.com>
2025-09-04 13:25:39 +0800

    Initial submission

---
+++ 01 - Practice Asessments/Practice Assessment 1/SuperDigit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class SuperDigit {
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer (up to 18 digits): ");
+        int num = scanner.nextInt();
+        int original = num;
+
+        while (num >= 10) {
+            long num = 0;
+            while (numb > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+        }
+    }
+}
